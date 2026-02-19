31d67c355664af9857ea91e6a54db6d2c00f375c
Denwien <denwien@users.noreply.github.com>
2026-02-19 20:06:27 +0300

    Try blocking pytest-dotenv with multiple methods and names

--- conftest.py
+++ conftest.py
@@ -1,10 +1,9 @@
-import os
-import sys
-
-os.environ.setdefault("PYTEST_DISABLE_PLUGIN_AUTOLOAD", "1")
+def pytest_addoption(parser):
+    pass
 
 def pytest_configure(config):
     try:
         config.pluginmanager.set_blocked("pytest_dotenv")
+        config.pluginmanager.set_blocked("pytest-dotenv")
     except Exception:
         pass
