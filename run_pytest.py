741264ded2d0bf3d16e656ee9d0fb0b8ac087a32
Denwien <denwien@users.noreply.github.com>
2026-02-19 20:30:53 +0300

    Add error handling to run_pytest.py for import

--- run_pytest.py
+++ run_pytest.py
@@ -6,7 +6,11 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault("PYTEST_DISABLE_PLUGIN_AUTOLOAD", "1")
 
-import pytest_plugin_blocker
+try:
+    import pytest_plugin_blocker
+except ImportError:
+    pass
+
 import pytest
 
 if __name__ == "__main__":
