556f18270a7584d311eba18a1429a5ea69415486
Denwien <denwien@users.noreply.github.com>
2026-02-19 20:30:28 +0300

    Improve run_pytest.py to handle arguments and ensure proper path

--- run_pytest.py
+++ run_pytest.py
@@ -2,10 +2,13 @@
 import sys
 import os
 
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 os.environ.setdefault("PYTEST_DISABLE_PLUGIN_AUTOLOAD", "1")
 
 import pytest_plugin_blocker
 import pytest
 
 if __name__ == "__main__":
-    sys.exit(pytest.main(["-vv", "tests", "-p", "no:pytest_dotenv", "-p", "no:pytest-dotenv"] + sys.argv[1:]))
+    args = sys.argv[1:] if len(sys.argv) > 1 else ["-vv", "tests"]
+    sys.exit(pytest.main(["-p", "no:pytest_dotenv", "-p", "no:pytest-dotenv"] + args))
