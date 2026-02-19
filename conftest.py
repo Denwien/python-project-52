7d65c17e9fbb8e07e7a87221091a9444f9331d60
Denwien <denwien@users.noreply.github.com>
2026-02-19 19:44:06 +0300

    Fix conftest.py to properly block pytest-dotenv plugin

--- conftest.py
+++ conftest.py
@@ -1,8 +1,5 @@
-import sys
-
-pytest_plugins = []
-
-for plugin in list(sys.modules.keys()):
-    if "pytest_dotenv" in plugin:
-        if "pytest_dotenv" in sys.modules:
-            del sys.modules["pytest_dotenv"]
+def pytest_configure(config):
+    try:
+        config.pluginmanager.set_blocked("pytest_dotenv")
+    except Exception:
+        pass
