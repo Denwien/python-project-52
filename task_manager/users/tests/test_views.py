bc2e2031dccaf90655f03cca6559880cbe981ff6
Denwien <denisgrigoriev1973@gmail.com>
2026-02-25 14:45:47 +0100

    Fix lint errors in users views tests

--- task_manager/users/tests/test_views.py
+++ task_manager/users/tests/test_views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 @pytest.mark.django_db
 def test_users_list_available_for_anonymous(client):
     """
-    User list is доступен анонимным пользователям.
+    User list доступен анонимным пользователям.
     LoginRequiredMixin не используется → 200 OK
     """
     response = client.get(reverse("users"))
@@ -15,7 +15,7 @@ def test_users_list_available_for_anonymous(client):
 
 @pytest.mark.django_db
 def test_users_list_logged_in(client):
-    user = User.objects.create_user(username="user", password="12345")
+    User.objects.create_user(username="user", password="12345")
     client.login(username="user", password="12345")
 
     response = client.get(reverse("users"))
@@ -28,7 +28,7 @@ def test_user_update_forbidden_for_other_user(client):
     UserPassesTestMixin → PermissionDenied → 403
     """
     owner = User.objects.create_user(username="owner", password="12345")
-    other = User.objects.create_user(username="other", password="12345")
+    User.objects.create_user(username="other", password="12345")
 
     client.login(username="other", password="12345")
 
@@ -51,7 +51,7 @@ def test_user_delete_forbidden_for_other_user(client):
     UserPassesTestMixin → PermissionDenied → 403
     """
     owner = User.objects.create_user(username="owner", password="12345")
-    other = User.objects.create_user(username="other", password="12345")
+    User.objects.create_user(username="other", password="12345")
 
     client.login(username="other", password="12345")
 
